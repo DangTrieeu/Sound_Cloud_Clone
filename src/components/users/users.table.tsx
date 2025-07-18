bd28d5a4c992f77a6af52f13a180338c5cadb85d
DangTrieeu <dangtrieu.lb@gmail.com>
2025-07-18 16:38:07 +0700

    #51 render list users

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,8 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../../styles/users.css";
 
+interface IUser {
+  email: string;
+  name: string;
+  role: string;
+}
+
 const UsersTable = () => {
-  const getData = async () => {
+  const [listUsers, setListUsers] = useState([]);
+  const getUserLogin = async () => {
     const responseLogin = await fetch(
       "http://localhost:8000/api/v1/auth/login",
       {
@@ -18,6 +25,8 @@ const UsersTable = () => {
     );
     const data = await responseLogin.json();
     console.log("Data fetched:", data);
+  };
+  const getListUsers = async () => {
     const access_token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjg3OWY2NjgzZTZiYjgyOTMxOWUyYWY2IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3NTI4Mjc5OTQsImV4cCI6MTgzOTIyNzk5NH0.U7RH7ag6YYjl5Qw_nwk5SCAiJoQnQ_3mivGO4EVVdO0";
     const responseListUsers = await fetch(
@@ -31,52 +40,38 @@ const UsersTable = () => {
       }
     );
     const dataListUsers = await responseListUsers.json();
-    console.log("List of users:", dataListUsers);
+    console.log("List of users:", dataListUsers.data.result);
+    setListUsers(dataListUsers.data.result);
   };
-  useEffect(() => {
-    getData();
-  }, []);
 
+  useEffect(() => {
+    //getUserLogin();
+    getListUsers();
+  }, []);
+  console.log("List of users:", listUsers);
   return (
     <div>
-      <h2>HTML Table</h2>
+      <h2>Users Table</h2>
 
       <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-        <tr>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers.map((item: IUser, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.email}</td>
+                <td>{item.name}</td>
+                <td>{item.role}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
