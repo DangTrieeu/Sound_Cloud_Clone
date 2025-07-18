ed350b81c6c6ff144440bdaf4e0c16e78f462ae5
DangTrieeu <dangtrieu.lb@gmail.com>
2025-07-18 16:08:24 +0700

    #50 call API with fetch

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,6 +1,42 @@
+import { useEffect } from "react";
 import "../../styles/users.css";
 
 const UsersTable = () => {
+  const getData = async () => {
+    const responseLogin = await fetch(
+      "http://localhost:8000/api/v1/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "hoidanit@gmail.com",
+          password: "123456",
+        }),
+      }
+    );
+    const data = await responseLogin.json();
+    console.log("Data fetched:", data);
+    const access_token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjg3OWY2NjgzZTZiYjgyOTMxOWUyYWY2IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3NTI4Mjc5OTQsImV4cCI6MTgzOTIyNzk5NH0.U7RH7ag6YYjl5Qw_nwk5SCAiJoQnQ_3mivGO4EVVdO0";
+    const responseListUsers = await fetch(
+      "http://localhost:8000/api/v1/users/all",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const dataListUsers = await responseListUsers.json();
+    console.log("List of users:", dataListUsers);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <h2>HTML Table</h2>
