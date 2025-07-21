0b4d29dc0696cff3f54d1472e1abb1d885bc390d
DangTrieeu <dangtrieu.lb@gmail.com>
2025-07-21 17:27:33 +0700

    routing with nextjs

---
+++ nextjs-mui-ts-starter/src/app/playlist/page.tsx
@@ -0,0 +1,10 @@
+
+const PlaylistPage = () => {
+    return (
+        <div>
+            <h1>Playlist Page</h1>
+            <p>This is the playlist page where users can manage their playlists.</p>
+        </div>
+    );
+}
+export default PlaylistPage;
