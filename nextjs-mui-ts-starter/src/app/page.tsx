f71220a26c20b3de0318661de39aef9082736694
DangTrieeu <dangtrieu.lb@gmail.com>
2025-07-22 09:11:40 +0700

    #91 design App bar

--- nextjs-mui-ts-starter/src/app/page.tsx
+++ nextjs-mui-ts-starter/src/app/page.tsx
@@ -1,7 +1,9 @@
+import AppHeader from "@/components/header/app.header";
+import App from "next/app";
 
 export default function HomePage() {
   return (
-    <div>hello word</div>
+    <AppHeader></AppHeader>
   );
 }
 
