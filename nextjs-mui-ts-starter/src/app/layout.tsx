4f85db00c82b4ccdd41a429a52cc83691e8080d3
DangTrieeu <dangtrieu.lb@gmail.com>
2025-07-22 11:50:41 +0700

    design footer

--- nextjs-mui-ts-starter/src/app/layout.tsx
+++ nextjs-mui-ts-starter/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ThemeRegistry from '@/components/theme-registry/theme.registry';
 import AppHeader from '@/components/header/app.header';
+import AppFooter from '@/components/footer/app.footer';
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -9,8 +10,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <ThemeRegistry>
           <AppHeader />
-
           {children}
+          <AppFooter />
         </ThemeRegistry>
       </body>
     </html>
