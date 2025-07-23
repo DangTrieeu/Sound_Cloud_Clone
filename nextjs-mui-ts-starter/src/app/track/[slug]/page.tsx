b4f51dd7903f984e8843b1a9d2b53ea9c9e81666
DangTrieeu <dangtrieu.lb@gmail.com>
2025-07-23 08:01:32 +0700

    search params

--- nextjs-mui-ts-starter/src/app/track/[slug]/page.tsx
+++ nextjs-mui-ts-starter/src/app/track/[slug]/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
 
 const DetailTrackPage = ({
     params,
 }: {
     params: Promise<{ slug: string }>
 }) => {
+    const searchParams = useSearchParams()
+    //lay theo key truyen tren url (o day key la audio)
+    const search = searchParams.get('audio')
+    //console.log(">>>check search param: ", search);
+
     return (
         <div> detail track page</div>
     )
--- nextjs-mui-ts-starter/src/components/main/main.slider.tsx
+++ nextjs-mui-ts-starter/src/components/main/main.slider.tsx
@@ -97,7 +97,7 @@ const MainSlider = (props: IProps) => {
                         return (
                             <div className="tracks" key={track._id}>
                                 <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/${track.imgUrl}`} alt="img" />
-                                <Link href={`/track/${track._id}`}>
+                                <Link href={`/track/${track._id}?audio=${track.trackUrl}`}>
                                     <h4>{track.title}</h4>
                                 </Link>
                                 <h5>{track.description}</h5>
